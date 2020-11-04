f1591aba831445e038addcb4933e00b989cc5479
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-04 20:40:43 +0530

    error handled in pipe by catch error operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { StringMapWithRename } from '@angular/compiler/src/compiler_facade_interface';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -34,6 +34,9 @@ export class postsService{
       //meken observable ekak return karanawa 
       //eka subscribe karala nathnam me request eka yanne ne
       //kauruth interest nathnam request eka yawanne ne e nisa kohe hari subscribe karala tyenna ona
+      //pipe eke map wagema thawa catchError kiyala function ekak nuth danna pluwn.
+      //fetch ekedi ena error ekak handle karanna ona una nam methenta catchError kiyanna eka danawa
+      //
     return this.http.get<{[key: string]: Post}>('https://ng-complete-guide-4fb9a.firebaseio.com/posts.json')
     .pipe(map(responsedata=>{
       const postArray: Post[] = [];//post object tyna array ekak
@@ -43,6 +46,15 @@ export class postsService{
         }
       }
       return postArray;
+        }),
+        catchError((errorRes)=>{
+          //send to analytic server//subject ekak dana error message eka next karannath ahaki
+          //eka wena thanaka subscribe karala e error eka ganna pluwn
+          //error ekak awama karanna ona background stuff methana karanna pluwn ex log karanna ,background server ekakata yawanna
+          //e karana ewa kalata passee "throwError" kiyala observable ekakin error eka ain karala danawa 
+          //throw karanawa
+          //Generic error handling task ekak karanna ona nam eka thama methana karanne
+          return throwError(errorRes);
         })
       );
     }
