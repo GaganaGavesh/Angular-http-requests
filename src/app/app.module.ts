8a25132620319b30a5a5e6bcd72f6d3b5444779e
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-06 09:08:56 +0530

    Register the interceptor in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';//meka thama http request walata ganne
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';//meka thama http request walata ganne
 //whole project ekata http enable karanawa
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth.interceptor.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,11 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, 
+    useClass: AuthInterceptorService, 
+    multi: true}],//dan angular eken request ekak leave wenakota interceptor method eka run wela e ethod eke tyna 
+    //request header yawana wade karanawa
   bootstrap: [AppComponent]
 })
 export class AppModule { }
